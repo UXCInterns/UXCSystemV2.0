d13edb2caad2a260dc720dfd35b169aa7aa619ae
Gladiator0611 <Harshansankar89@gmail.com>
2026-01-29 15:22:39 +0800

    Fixed all errors

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -1,11 +1,16 @@
 import SignInForm from "@/components/auth/SignInForm";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Signin Page TailAdmin Dashboard Template",
+  title: "SignIn Page | UXC System",
+  description: "SignIn to UXC System",
 };
 
 export default function SignIn() {
-  return <SignInForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignInForm />
+    </Suspense>
+  );
 }
