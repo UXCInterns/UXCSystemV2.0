5dc464f9f409c42c3c0f7d759e6f0635c8a385bc
fukai33 <fukai33.24@ichat.sp.edu.sg>
2026-06-12 11:16:53 +0800

    moved down the 3 boxex for total, unique companies visited and multiple times to 3 long stacking box

--- src/components/dashboard/StatsMetrics/stats.tsx
+++ src/components/dashboard/StatsMetrics/stats.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import React from "react";
-import { GroupIcon, ShootingStarIcon, BoltIcon } from "@/icons";
 import { useMetricsData } from "@/hooks/learningJourney/DashboardComponents/useMetricsData";
 // import { MetricsHeader } from "./MetricsHeader";
-import { LogoCard } from "./LogoCard";
 import { MetricCard } from "./MetricCard";
 import { createMetricBadge } from "./MetricBadgeFactory";
 import { MultipleVisitsBadge } from "./MultipleVisitsBadge";
@@ -34,11 +32,10 @@ export const Stats2= () => {
       )}
 
       {/* Metrics Grid */}
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 md:gap-6">
-        <LogoCard />
+      <div className="grid grid-cols-1 gap-4">
 
         <MetricCard
-          icon={<GroupIcon className="text-gray-800 size-6 dark:text-white/90" />}
+          icon={null}
           title="Total Companies"
           value={totalCompanies.toString()}
           badge={createMetricBadge(
@@ -53,7 +50,7 @@ export const Stats2= () => {
         />
 
         <MetricCard
-          icon={<ShootingStarIcon className="text-gray-800 dark:text-white/90" />}
+          icon={null}
           title="Unique Companies"
           value={uniqueCompanies.length.toString()}
           badge={createMetricBadge(
@@ -68,7 +65,7 @@ export const Stats2= () => {
         />
 
         <MetricCard
-          icon={<BoltIcon className="text-gray-800 dark:text-white/90" />}
+          icon={null}
           title="Visited Multiple Times"
           value={multipleVisits.length.toString()}
           badge={
